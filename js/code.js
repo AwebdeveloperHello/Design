cd0d668e00ead67b5858fbcc8f12b9250ac345e4
Tolik <155878002+AwebdeveloperHello@users.noreply.github.com>
2026-01-03 13:04:11 +0300

    Project

---
+++ js/code.js
@@ -0,0 +1,24 @@
+const line = document.getElementById("line");
+
+let start = null;
+const duration = 1200;
+
+function easeInOut(t) {
+    return t < 0.5
+        ? 2 * t * t
+        : 1 - Math.pow(-2 * t + 2, 2) / 2;
+}
+
+function animate(timestamp) {
+    if (!start) start = timestamp;
+    const progress = Math.min((timestamp - start) / duration, 1);
+    const eased = easeInOut(progress);
+
+    line.style.transform = `scaleX(${eased})`;
+
+    if (progress < 1) {
+        requestAnimationFrame(animate);
+    }
+}
+
+requestAnimationFrame(animate);
